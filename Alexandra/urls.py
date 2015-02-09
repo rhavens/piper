a97a4ca1c8534e43b4b61d7a2b02808fb97565bf
jwde <jaydestories@gmail.com>
2015-02-09 17:35:03 -0500

    Server now delivers posts and users

--- Alexandra/urls.py
+++ Alexandra/urls.py
@@ -3,4 +3,5 @@ from Alexandra import views
 
 urlpatterns = patterns ( '',
   url(r'^$', views.index, name = 'index' ),
+  url(r'^posts/', views.posts, name = 'posts'),
   )
--- Alexandra/views.py
+++ Alexandra/views.py
@@ -4,15 +4,15 @@ from django.core import serializers
 from .models import User
 from .models import Post
 
-#def index(request):
-#  user = User.objects.all()
-#  #names = ",".join(map(lamda x:x.username, user))
-#  data = serializers.serialize('json', user)
-#  return HttpResponse(data)
+def index(request):
+  user = User.objects.all()
+  #names = ",".join(map(lamda x:x.username, user))
+  data = serializers.serialize('json', user)
+  return HttpResponse(data)
 
 
 def posts(request):
-  latest_posts = Post.objects.order_by(id)
+  latest_posts = Post.objects.order_by('id')
   context = {'latest_posts' : latest_posts}
   return render(request,'Alexandra/index.html',  context)
   
