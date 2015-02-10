c3e15e7903a88f5daf0aa0c2d30df48584d9123a
jwde <jaydestories@gmail.com>
2015-02-09 20:49:26 -0500

    Added model for postform

--- Alexandra/models.py
+++ Alexandra/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-
-from django.db import models
+from django.forms import ModelForm
+from django.utils.translation import ugettext_lazy as _
 
 #class User(models.Model):
 #    id = models.AutoField(primary_key=True)
@@ -23,6 +21,7 @@ from django.db import models
 class Post (models.Model):
     id = models.AutoField(primary_key=True)
     text_content = models.CharField(max_length = 200)
+# for the mvp, perhaps just store a link instead of an s3 key
     image_key = models.CharField (max_length = 1024)
     created_at = models.DateTimeField(editable=False)
     last_modified = models.DateTimeField()
@@ -34,7 +33,24 @@ class Post (models.Model):
     class Meta:
         get_latest_by = 'last_modified'
         ordering = ['-last_modified']
-    
+
+class PostForm (ModelForm):
+    class Meta:
+        model = Post
+        fields = ['text_content', 'image_key']
+        labels = {
+            'text_content': _('Description'),
+            'image_key': _('Image'),
+        }
+        error_messages = {
+            'text_content': {
+                'max_length': _('Character limit is 200 characters'),
+            },
+        }
+        widgets = {
+            'text_content': Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
+
 #class Comment (models.Model):
 #    id = models.AutoField(primary_key=True)
 #    username = models.CharField(max_length = 60)
