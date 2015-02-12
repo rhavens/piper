992745212f6337977834cdbfbd46050372488e75
Ryan Havens <ryan.d.havens@gmail.com>
2015-02-11 19:35:38 -0500

    Updated post styling

--- Alexandra/urls.py
+++ Alexandra/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns , url
 from Alexandra import views
 
 urlpatterns = patterns ( '',
-    url(r'^$', views.index, name = 'index' ),
+    url(r'^$', views.posts, name = 'index' ),
     url(r'^posts/', views.posts, name = 'posts'),
     url(r'^post/(?P<post_id>\d+)', views.post, name = 'post'),
     url(r'^new_post/', views.new_post, name = 'new_post'),
