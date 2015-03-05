905fd597c437e25e8d9aa8d78d485b6e0a9a062b
jwde <jaydestories@gmail.com>
2015-03-04 20:47:09 -0500

    Updated fab

--- fabfile/project_conf.py
+++ fabfile/project_conf.py
@@ -21,7 +21,7 @@ fabconf = {}
 fabconf['FAB_CONFIG_PATH'] = os.path.dirname(__file__)
 
 # Project name
-fabconf['PROJECT_NAME'] = "amazon_app"
+fabconf['PROJECT_NAME'] = "Project_120"
 
 # Username for connecting to EC2 instaces - Do not edit unless you have a reason to
 fabconf['SERVER_USERNAME'] = "ubuntu"
@@ -30,7 +30,7 @@ fabconf['SERVER_USERNAME'] = "ubuntu"
 fabconf['SSH_PATH'] = "~/.ssh"
 
 # Name of the private key file you use to connect to EC2 instances
-fabconf['EC2_KEY_NAME'] = "my_ssh_key.pem"
+fabconf['EC2_KEY_NAME'] = "piper.pem"
 
 # Don't edit. Full path of the ssh key you use to connect to EC2 instances
 fabconf['SSH_PRIVATE_KEY_PATH'] = '%s/%s' % (fabconf['SSH_PATH'], fabconf['EC2_KEY_NAME'])
@@ -42,7 +42,7 @@ fabconf['APPS_DIR'] = "/home/%s/webapps" % fabconf['SERVER_USERNAME']
 fabconf['PROJECT_PATH'] = "%s/%s" % (fabconf['APPS_DIR'], fabconf['PROJECT_NAME'])
 
 # App domains
-fabconf['DOMAINS'] = "example.com www.example.com"
+fabconf['DOMAINS'] = "piper.link www.piper.link"
 
 # Path for virtualenvs
 fabconf['VIRTUALENV_DIR'] = "/home/%s/.virtualenvs" % fabconf['SERVER_USERNAME']
@@ -51,7 +51,7 @@ fabconf['VIRTUALENV_DIR'] = "/home/%s/.virtualenvs" % fabconf['SERVER_USERNAME']
 fabconf['ADMIN_EMAIL'] = "admin@example.com"
 
 # Git username for the server
-fabconf['GIT_USERNAME'] = "EC2"
+fabconf['GIT_USERNAME'] = "piperdeploy"
 
 # Name of the private key file used for github deployments
 fabconf['BITBUCKET_DEPLOY_KEY_NAME'] = "bitbucket_rsa"
@@ -60,8 +60,8 @@ fabconf['BITBUCKET_DEPLOY_KEY_NAME'] = "bitbucket_rsa"
 fabconf['BITBUCKET_DEPLOY_KEY_PATH'] = "%s/%s" % (fabconf['SSH_PATH'], fabconf['BITBUCKET_DEPLOY_KEY_NAME'])
 
 # Path to the repo of the application you want to install
-fabconf['BITBUCKET_USERNAME'] = ''
-fabconf['BITBUCKET_REPO_NAME'] = ''
+fabconf['BITBUCKET_USERNAME'] = 'piperdeploy'
+fabconf['BITBUCKET_REPO_NAME'] = 'piperdeploy'
 
 # Creates the ssh location of your bitbucket repo from the above details
 fabconf['BITBUCKET_REPO'] = "ssh://git@bitbucket.org/%s/%s.git" % (fabconf['BITBUCKET_USERNAME'], fabconf['BITBUCKET_REPO_NAME'])
@@ -70,25 +70,25 @@ fabconf['BITBUCKET_REPO'] = "ssh://git@bitbucket.org/%s/%s.git" % (fabconf['BITB
 fabconf['ACTIVATE'] = "source /home/%s/.virtualenvs/%s/bin/activate" % (fabconf['SERVER_USERNAME'], fabconf['PROJECT_NAME'])
 
 # Name tag for your server instance on EC2
-fabconf['INSTANCE_NAME_TAG'] = "MyInstance"
+fabconf['INSTANCE_NAME_TAG'] = "Piper"
 
 # EC2 key. http://bit.ly/j5ImEZ
-fabconf['AWS_ACCESS_KEY'] = ''
+fabconf['AWS_ACCESS_KEY'] = 'AKIAJ5G3P56SBQ5CDIAQ'
 
 # EC2 secret. http://bit.ly/j5ImEZ
-fabconf['AWS_SECRET_KEY'] = ''
+fabconf['AWS_SECRET_KEY'] = 'P0nZOa+OubkLLRnD+6XsNHg1aXadUnaNQPmB8DSE'
 
 #EC2 region. http://amzn.to/12jBkm7
-ec2_region = 'ap-southeast-2'
+ec2_region = 'ap-east-1b'
 
 # AMI name. http://bit.ly/liLKxj
 ec2_amis = ['ami-51821b6b']
 
 # Name of the keypair you use in EC2. http://bit.ly/ldw0HZ
-ec2_keypair = 'insert_keypair_name'
+ec2_keypair = 'piper'
 
 # Name of the security group. http://bit.ly/kl0Jyn
-ec2_secgroups = ['MySecurityGroup']
+ec2_secgroups = ['Pipersec']
 
 # API Name of instance type. http://bit.ly/mkWvpn
 ec2_instancetype = 't1.micro'
