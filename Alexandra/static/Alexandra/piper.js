2cb92d70751c4ed10280b0dcb31ecb6dd66a99e1
jwde <jaydestories@gmail.com>
2015-04-02 12:49:29 -0400

    unshift instead of +

--- Alexandra/static/Alexandra/piper.js
+++ Alexandra/static/Alexandra/piper.js
@@ -58,7 +58,7 @@ function PollWorker() {
                 for (j = 0; updates['objects'][j].id != recId && j < updates['objects'].length; ++j);
                 for (k = j - 1; k >= 0; --k) { 
                     RenderPost(updates['objects'][i]);
-                    posts = updates['objects'][i] + posts;
+                    posts.unshift(updates['objects'][i];
                 }
             }
         },
