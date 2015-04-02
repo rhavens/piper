7f22880d224c7ba0c145163d79b01b9f3eb28c0f
jwde <jaydestories@gmail.com>
2015-04-01 22:53:02 -0400

    Live polling test

---
+++ Alexandra/static/Alexandra/piper.js
@@ -0,0 +1,74 @@
+var posts = null;
+$(document).ready(function () {
+    window.applicationCache.onupdateready = function(e) {
+        window.applicationCache.update();
+    }
+    $('#id_image').hide();
+    $('#new_post_submit').hide();
+    $('.ui.sticky').sticky();
+    $('#newpostbutton').click(function () {
+        $('#newpostmodal').modal('show');
+    });
+    $('#id_image_styled').click(function () {
+        $('#id_image').click();
+    });
+    $('#new_post_submit_styled').click(function () {
+        $('#new_post_submit').click();
+    });
+    $('#id_image').change( function () {
+        var fileName = $(this).val().split('/').pop().split('\\').pop();
+        if (fileName) {
+            $('#id_image_styled').html("<div>" + fileName + "</div>");
+        } else {
+            $('#id_image_styled').html("<div>Upload an image</div>");
+        }
+    });
+});
+function PollWorker() {
+    $.ajax({
+        url: 'http://www.piper.link/api/posts/post/?format=json',
+        success: function(response) {
+            var updates = JSON.parse(response);
+            if (posts === null) {
+                posts = [];
+                for (i = updates['objects'].length - 1; i >= 0; --i) {
+                    RenderPost(updates['objects'][i]);
+                    posts = updates['objects'][i] + posts;
+                }
+            } else {
+                var recId = posts[0].id;
+                var j;
+                for (j = 0; updates['objects'][j].id != recId && j < updates['objects'].length; ++j);
+                for (k = j - 1; k >= 0; --k) { 
+                    RenderPost(updates['objects'][i]);
+                    posts = updates['objects'][i] + posts;
+                }
+            }
+        },
+        complete: function() {
+            setTimeout(PollWorker, 2000);
+        }
+    });
+};
+PollWorker();
+function LoadSinglePost(postId) {
+    $('#singlepostimage').attr('src', $('#postimage' + postId).attr('src'));
+    $('#singlepostdescription').html($('#posttext' + postId).html());
+    $('#singlepostmodal.ui.modal').modal('show');
+}
+function RenderPost(post) {
+    var posthtml = 
+    "<div class='ui vertical segment'>" +
+        "<div class='content'>";
+    var postimagesplit = post.image.split("/");
+    if (postimagesplit[postimagesplit.length - 1] != "NULL") {
+        posthtml +=
+            "<a href='javascript:void(0);' onclick='LoadSinglePost(" + post['id'] + ");return false;'><img id='postimage" + post['id'] + "' class='ui image center' src='" + post['image'] + "' alt='user image'/></a>";
+    }
+    posthtml +=
+    "<p id='posttext" + post['id'] + "'>" + post['text_content'] + "</p>" +
+    "</div></div>"; 
+
+    $('#postlist').html(posthtml + $('#postlist').html());
+}
+
