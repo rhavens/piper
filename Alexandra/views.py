5fa6b4e57b9e029634cacfb3b76a54a5aa182c03
jwde <jaydestories@gmail.com>
2015-04-27 21:05:56 -0400

    Changed comment pop to index remove

--- Alexandra/views.py
+++ Alexandra/views.py
@@ -39,8 +39,8 @@ def posts(request):
 #                                      id=(User_post.objects.get(post=x.id).user)).username},
 #                            latest_posts)
     
-#    comments = Comments.objects.order_by('-created_at')[:10:-1]
-    comments = Comments.objects.order_by('created_at')[:10]
+    comments = Comments.objects.order_by('-created_at')[:10:-1]
+    comments = reversed(comments)
     context = {'latest_posts':latest_posts,
                 'form':PostForm(),
                 'comments': comments, 
